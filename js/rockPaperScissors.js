be4b4fca91b38b548b5f13c6e94e18057cf1fde1
arthurfelixgr <arthurfelixgr@gmail.com>
2019-12-24 22:39:53 -0300

    Create rockPaperScissors.js

---
+++ js/rockPaperScissors.js
@@ -0,0 +1,70 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInut === 'egg'){
+    return userInput;
+  } else {
+    return 'Please, insert a valid option.';
+  }
+};
+  
+function getComputerChoice(){
+  switch(Math.floor(Math.random() *3)){
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    default:
+      return 'scissors';
+      break;
+  }
+  /*
+  var num = Math.floor(Math.random() *3);
+  if(num === 0){
+    return 'rock';
+  } else if(num === 1){
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+  */
+}
+  
+function determineWinner(userChoice, computerChoice){
+  if(userChoice === computerChoice){
+    return 'The game was a tie!';
+  } else if(userChoice === 'egg'){
+    return 'You won!';
+  } else if (userChoice === 'rock'){
+    if(computerChoice === 'paper'){
+      return 'The computer won.'
+    } else {
+      return 'You won!';
+    }
+  } else if(userChoice === 'paper'){
+    if(computerChoice === 'scissors'){
+      return 'The computer won.';
+    } else {
+      return 'You won!';
+    }
+  } else if(userChoice === 'scissors'){
+    if(computerChoice === 'rock'){
+      return 'The computer won.';
+    } else {
+      return 'You won!';
+    }
+  } else {
+    return 'Please, insert a valid option.';
+  }
+}
+
+function playGame(){
+  const userChoice = getUserChoice('scissors');
+  const computerChoice = getComputerChoice();
+  console.log('You threw: '+userChoice);
+  console.log('The computer threw: '+computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
