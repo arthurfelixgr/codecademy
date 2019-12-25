ea5610d603ba594eab3180e003c49a0ba5952997
arthurfelixgr <arthurfelixgr@gmail.com>
2019-12-24 22:47:08 -0300

    Create pedraPapelTesoura.js

---
+++ js/pedraPapelTesoura.js
@@ -0,0 +1,70 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'pedra' || userInput === 'papel' || userInput === 'tesoura' || userInut === 'ovo'){
+    return userInput;
+  } else {
+    return 'Por favor, insira um opção válida.';
+  }
+};
+  
+function getComputerChoice(){
+  switch(Math.floor(Math.random() *3)){
+    case 0:
+      return 'pedra';
+      break;
+    case 1:
+      return 'papel';
+      break;
+    default:
+      return 'tesoura';
+      break;
+  }
+  /*
+  var num = Math.floor(Math.random() *3);
+  if(num === 0){
+    return 'pedra';
+  } else if(num === 1){
+    return 'papel';
+  } else {
+    return 'tesoura';
+  }
+  */
+}
+  
+function determineWinner(userChoice, computerChoice){
+  if(userChoice === computerChoice){
+    return 'Empate!';
+  } else if(userChoice === 'ovo'){
+    return 'Você venceu!';
+  } else if (userChoice === 'pedra'){
+    if(computerChoice === 'papel'){
+      return 'O computador venceu.'
+    } else {
+      return 'Você venceu!';
+    }
+  } else if(userChoice === 'papel'){
+    if(computerChoice === 'tesoura'){
+      return 'O computador venceu.';
+    } else {
+      return 'Você venceu!';
+    }
+  } else if(userChoice === 'tesoura'){
+    if(computerChoice === 'pedra'){
+      return 'O computador venceu.';
+    } else {
+      return 'Você venceu!';
+    }
+  } else {
+    return 'Por favor, insira um opção válida.';
+  }
+}
+
+function playGame(){
+  const userChoice = getUserChoice('tesoura');
+  const computerChoice = getComputerChoice();
+  console.log('Você escolheu: '+userChoice);
+  console.log('O computador escolheu: '+computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
